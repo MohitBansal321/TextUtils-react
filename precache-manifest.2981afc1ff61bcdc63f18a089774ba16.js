7197613b1e92dc21ffe957880b5386f8fdd555c2
Mohit Bansal <mohit1291@chitkara.edu.in>
2022-10-21 00:49:14 +0530

    Updates

---
+++ precache-manifest.2981afc1ff61bcdc63f18a089774ba16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "182136ad76dfd340a6e3",
+    "url": "/TextUtils-react/static/css/main.9c2be6e9.chunk.css"
+  },
+  {
+    "revision": "182136ad76dfd340a6e3",
+    "url": "/TextUtils-react/static/js/main.7d8eaec5.chunk.js"
+  },
+  {
+    "revision": "31e60ec7500eb428c5b1",
+    "url": "/TextUtils-react/static/js/runtime~main.f46ce029.js"
+  },
+  {
+    "revision": "9162cf41c7fa6eafdcbb",
+    "url": "/TextUtils-react/static/js/2.518c7694.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/TextUtils-react/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "d34879b0c62bca950b3733e74aa5778c",
+    "url": "/TextUtils-react/index.html"
+  }
+];
